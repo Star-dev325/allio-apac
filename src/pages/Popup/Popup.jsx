bb50f08002de293d7f23725c1b9c11d8c93ccb4d
Star <leonwang325@gmail.com>
2023-03-09 16:28:09 -0500

    completed popup with statistic reports

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import $ from 'jquery';
+
 import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import Reports from './Reports';
+import './Popup.scss';
+
+let htmlSource = '';
 
 const Popup = () => {
+
+  const [isPending, setIsPending] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isGlipseMode, setIsGlipseMode] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <Greetings />
+      <div className='info-sector'>
+        {isGlipseMode ?
+          <div className='row-container'>
+            <Reports source={htmlSource} />
+          </div>
+          : <div className='row-container column'>
+            <Button onClick={() => {
+              setIsPending(true);
+              chrome.tabs.query({ active: true }, tabs => {
+                const pageURL = tabs[0].url;
+                try {
+                  $.get(pageURL, htmlStr => {
+                    setIsPending(false);
+                    if (htmlStr) setIsSuccess(true);
+                    htmlSource = htmlStr;
+                  });
+                } catch (e) {
+                  setIsSuccess(false);
+                  console.log('Error message: ', e.message);
+                };
+              });
+            }} variant='contained' disabled={isPending}>
+              {isPending && <CircularProgress size={24} />} {isSuccess ? 'Parse again' : 'Parse this page'}
+            </Button>
+            {isSuccess && <div className='row-container fill-space'>
+              <h2>{isSuccess ? 'Scraping HTML has been done successfully!' : 'Error ocurred'}</h2>
+            </div>}
+          </div>}
+      </div>
+      <div className='control-sector'>
+        <Button onClick={() => {
+          chrome.tabs.create({ url: chrome.runtime.getURL('dashboard.html'), active: true });
+        }} color='info' disabled={!isSuccess}>Open in new tab</Button>
+        <Button onClick={() => {
+          setIsGlipseMode(true);
+        }} color='info' disabled={!isSuccess}>glimpse</Button>
+      </div>
     </div>
   );
 };

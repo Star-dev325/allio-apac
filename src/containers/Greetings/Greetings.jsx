db3505dfff96d7867108c433ac898c12bcfd7024
Star <leonwang325@gmail.com>
2023-03-09 13:38:08 -0500

    updated Greetings component

--- src/containers/Greetings/Greetings.jsx
+++ src/containers/Greetings/Greetings.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import icon from '../../assets/img/icon-128.png';
 
-class GreetingComponent extends Component {
-  state = {
-    name: 'dev',
-  };
-
-  render() {
-    return (
-      <div>
-        <p>Hello, {this.state.name}!</p>
-        <img src={icon} alt="extension icon" />
-      </div>
-    );
-  }
-}
+const GreetingComponent = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <h1>DOM Serializer/Parser</h1>
+    <img src={icon} alt="extension icon" />
+  </div>
+)
 
 export default GreetingComponent;

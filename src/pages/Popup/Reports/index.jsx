f732baa7c08bc2846485605fc1106b291d598b9e
Star <leonwang325@gmail.com>
2023-03-09 13:39:44 -0500

    created a Rports component

---
+++ src/pages/Popup/Reports/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import $ from 'jquery';
+
+import './index.css';
+import { InputLabel, TextField } from '@mui/material';
+
+
+const Reports = ({ htmlSource }) => {
+
+  return (
+    <div className='main'>
+    </div>
+  );
+};
+
+export default Reports;
